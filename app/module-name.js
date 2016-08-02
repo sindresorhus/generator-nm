9a3c40204f24b5190fbac3b43abe374b0ed709e0
Jonny Burger <jonathanburger11@gmail.com>
2016-08-02 23:57:18 +0200

    Make functions in app/module-name.js one-liners

--- app/module-name.js
+++ app/module-name.js
@@ -2,18 +2,6 @@ const _s = require('underscore.string');
 
 const scopedRegex = /^(?:@([^/]+?)[/])?([^/]+?)$/;
 
-exports.isScoped = name => {
-	return scopedRegex.test(name);
-};
-exports.repoName = name => {
-	if (exports.isScoped(name)) {
-		return name.match(scopedRegex)[2];
-	}
-	return name;
-};
-exports.slugify = name => {
-	if (exports.isScoped(name)) {
-		return name;
-	}
-	return _s.slugify(name);
-};
+exports.isScoped = name => scopedRegex.test(name);
+exports.repoName = name => exports.isScoped(name) ? name.match(scopedRegex)[2] : name;
+exports.slugify = name => exports.isScoped(name) ? name : _s.slugify(name);
