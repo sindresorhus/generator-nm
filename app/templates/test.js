505a8390a1ca959aee938b2d1f249d0bf38b80bb
Sindre Sorhus <sindresorhus@gmail.com>
2017-01-23 15:40:17 +0700

    Update test.js

--- app/templates/test.js
+++ app/templates/test.js
@@ -2,7 +2,8 @@ import test from 'ava';
 import fn from './';
 
 test('title', t => {
-	t.is(fn('unicorns'), 'unicorns & rainbows');
 	const err = t.throws(() => fn(123), TypeError);
 	t.is(err.message, 'Expected a string, got number');
+	
+	t.is(fn('unicorns'), 'unicorns & rainbows');
 });
