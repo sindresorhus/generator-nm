c17c8fadcedec0600b9b7240475c7bb124a46d0c
Luke Edwards <luke.edwards05@gmail.com>
2017-01-23 00:40:38 -0800

    Add `throw TypeError` test (#64)

--- app/templates/test.js
+++ app/templates/test.js
@@ -2,5 +2,8 @@ import test from 'ava';
 import fn from './';
 
 test('title', t => {
+	const err = t.throws(() => fn(123), TypeError);
+	t.is(err.message, 'Expected a string, got number');
+	
 	t.is(fn('unicorns'), 'unicorns & rainbows');
 });
