f4f84ac6b34016a3bed502dac4a81445fd7d39f3
Luke Edwards <luke.edwards05@gmail.com>
2017-01-22 16:07:00 -0800

    add `throw TypeError` test
    Illustrates AVA error testing for beginners. Also bumps NYC coverage to 100%.

--- app/templates/test.js
+++ app/templates/test.js
@@ -3,4 +3,6 @@ import fn from './';
 
 test('title', t => {
 	t.is(fn('unicorns'), 'unicorns & rainbows');
+	const err = t.throws(() => fn(123), TypeError);
+	t.is(err.message, 'Expected a string, got number');
 });
