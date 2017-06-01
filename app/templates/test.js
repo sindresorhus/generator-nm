82e26a0998d1a12f544921eb78ac0b715fcb7779
Sindre Sorhus <sindresorhus@gmail.com>
2017-06-02 02:18:52 +0700

    Meta tweaks

--- app/templates/test.js
+++ app/templates/test.js
@@ -2,7 +2,9 @@ import test from 'ava';
 import m from '.';
 
 test('title', t => {
-	const err = t.throws(() => m(123), TypeError);
+	const err = t.throws(() => {
+		m(123)
+	}, TypeError);
 	t.is(err.message, 'Expected a string, got number');
 
 	t.is(m('unicorns'), 'unicorns & rainbows');
