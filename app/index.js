c223e6b128b3778eae0179dde49ec1b9e973d58f
Sindre Sorhus <sindresorhus@gmail.com>
2018-10-28 12:06:15 +0700

    Upgrade dependencies

--- app/index.js
+++ app/index.js
@@ -40,7 +40,7 @@ module.exports = class extends Generator {
 		}, {
 			name: 'moduleDescription',
 			message: 'What is your module description?',
-			default: `My ${superb()} module`
+			default: `My ${superb.random()} module`
 		}, {
 			name: 'githubUsername',
 			message: 'What is your GitHub username?',
